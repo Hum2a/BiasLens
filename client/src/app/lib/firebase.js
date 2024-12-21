ae2426c24e031fd57f37bd1c5b0658e070bb70db
Humza Butt <Humzab1711@hotmail.com>
2024-12-21 23:25:21 +0000

    Bizarre Vercel Rules

--- client/src/app/lib/firebase.js
+++ client/src/app/lib/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCGJVoqErK2zin-VNCfVJ_cwziHH48vzs8",
@@ -13,7 +12,6 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getFirestore(app);
 
 export default db;
