48cf26366a76e29638ce25b199884443f3b361c3
Humza Butt <Humzab1711@hotmail.com>
2024-12-22 00:39:35 +0000

    This might work

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['https://biaslens.onrender.com'], // Add domains for external images
+  },
 };
 
 export default nextConfig;
